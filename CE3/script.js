2344e23de6692e4549cddd79b9ee0723fd2675ec
truhig <truhig@gmail.com>
2022-02-15 14:34:38 -0500

    initial upload to github

---
+++ CE3/script.js
@@ -0,0 +1,17 @@
+$(window).on("load", start);
+
+function start(){
+    $("ul.nav a:first").click();
+}
+
+$(document).on("click", "ul.nav a", nav)
+
+function nav(e){
+    e.preventDefault()
+    //alert("hello");
+    var url = $(this).attr("href");
+    $("#content").load(url);
+    $("ul.nav a").removeClass("active")
+    $(this).addClass("active");
+}
+
